f7e3ebc119dc0e10ee974c542428576fe9c0668d
jeremie-riis <jbornais@riis.com>
2024-07-10 22:09:12 -0400

    add basic plugin

---
+++ weather_iot_plugin/README.md
@@ -0,0 +1,38 @@
+ATAK Weather IOT Plugin
+
+_________________________________________________________________
+PURPOSE AND CAPABILITIES
+
+This plugin allows you to connect to and display data from IoT weather stations.
+
+_________________________________________________________________
+STATUS
+
+In Development
+
+_________________________________________________________________
+POINT OF CONTACTS
+
+If you would like to [contact RIIS](https://www.riis.com/contact) about potentially starting an ATAK project contact Godfrey (below).
+If you want to report something that's broken open an issue or if you want to fix something open a PR.
+
+RIIS
+- Godfrey Nolan (godfrey@riis.com)
+- Jeremie Bornais (jbornais@riis.com)
+
+_________________________________________________________________
+PORTS REQUIRED
+
+n/a
+
+_________________________________________________________________
+EQUIPMENT REQUIRED
+
+_________________________________________________________________
+EQUIPMENT SUPPORTED
+
+_________________________________________________________________
+COMPILATION
+
+_________________________________________________________________
+DEVELOPER NOTES
---
+++ weather_iot_plugin/app/src/main/java/com/atakmap/android/weatheriotplugin/WeatherIoTPluginMapComponent.java
@@ -0,0 +1,43 @@
+
+package com.atakmap.android.weatheriotplugin;
+
+import android.content.Context;
+import android.content.Intent;
+import com.atakmap.android.ipc.AtakBroadcast.DocumentedIntentFilter;
+
+import com.atakmap.android.maps.MapView;
+import com.atakmap.android.dropdown.DropDownMapComponent;
+
+import com.atakmap.coremap.log.Log;
+import com.atakmap.android.weatheriotplugin.plugin.R;
+
+public class WeatherIoTPluginMapComponent extends DropDownMapComponent {
+
+    private static final String TAG = "PluginTemplateMapComponent";
+
+    private Context pluginContext;
+
+    private WeatherIoTPluginDropDownReceiver ddr;
+
+    public void onCreate(final Context context, Intent intent,
+            final MapView view) {
+
+        context.setTheme(R.style.ATAKPluginTheme);
+        super.onCreate(context, intent, view);
+        pluginContext = context;
+
+        ddr = new WeatherIoTPluginDropDownReceiver(
+                view, context);
+
+        Log.d(TAG, "registering the plugin filter");
+        DocumentedIntentFilter ddFilter = new DocumentedIntentFilter();
+        ddFilter.addAction(WeatherIoTPluginDropDownReceiver.SHOW_PLUGIN);
+        registerDropDownReceiver(ddr, ddFilter);
+    }
+
+    @Override
+    protected void onDestroyImpl(Context context, MapView view) {
+        super.onDestroyImpl(context, view);
+    }
+
+}
